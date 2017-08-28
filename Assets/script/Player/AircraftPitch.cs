7503d8a9ddbc4e80637cc425f9363d035aefb26f
Roroah <kenshirooni@gmail.com>
2017-08-29 01:52:49 +0200

    init

---
+++ Assets/script/Player/AircraftPitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AircraftPitch : MonoBehaviour
+{
+
+	public Rigidbody rb;
+	public float maxRot = 10f;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.rotation = Quaternion.Euler(new Vector3 (rb.velocity.z*1.5f, 0, -rb.velocity.x*3));
+	}
+}
