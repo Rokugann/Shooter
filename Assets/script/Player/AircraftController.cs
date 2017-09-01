4c5b32d72d62a8cfdbbf6eb0ee06dd469ddd0da1
Rudy Parenti <rudy-parenti@aliengraphics.eu>
2017-09-01 13:33:36 +0200

    Changing moving method

--- Assets/script/Player/AircraftController.cs
+++ Assets/script/Player/AircraftController.cs
@@ -5,54 +5,37 @@ using UnityEngine;
 public class AircraftController : MonoBehaviour
 {
 
+//TODO Separate Shoot and Move
+
 	public GameObject blaster;
-	public float moveSpeed = 10f;
-	public float turnSpeed = 50f;
-	public float maxSpeed = 5f;
-	public float slowSpeed = 0.4f;
-	Transform[] shootPos;
-	Rigidbody rb;
+
+	[SerializeField]
+	private float moveSpeed = 10f;
+
+	[SerializeField]
+ 	private float turnSpeed = 50f, slowSpeed = 0.4f;
+
+
+	private Transform[] shootPos;
+	private Rigidbody rb;
 
 	// Use this for initialization
-	void Start ()
+	private void Start ()
 	{
 		rb = GetComponent <Rigidbody> ();
 		shootPos = new Transform[blaster.transform.childCount];
-		for (int i = 0; i < blaster.transform.childCount; i++) {
+		for (int i = 0; i < blaster.transform.childCount; i++)
 			shootPos [i] = blaster.transform.GetChild (i);
-		}
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	private void Update ()
 	{
-	}
+		Vector3 v = this.transform.position, nv = new Vector3 ();
 
-	void FixedUpdate ()
-	{
-		if (Input.GetKey (KeyCode.UpArrow) && rb.velocity.x > -maxSpeed)
-			rb.AddForce (transform.right * -moveSpeed);
-		else if (!Input.GetKey (KeyCode.UpArrow) && rb.velocity.x < 0f)
-			rb.AddForce (transform.right * (moveSpeed * slowSpeed));
-		
-		if (Input.GetKey (KeyCode.DownArrow) && rb.velocity.x < maxSpeed)
-			rb.AddForce (transform.right * moveSpeed);
-		else if (!Input.GetKey (KeyCode.DownArrow) && rb.velocity.x > 0f)
-			rb.AddForce (transform.right * -(moveSpeed * slowSpeed));
-		
-		if (Input.GetKey (KeyCode.LeftArrow) && rb.velocity.z > -maxSpeed)
-			rb.AddForce (transform.forward * -moveSpeed);
-		else if (!Input.GetKey (KeyCode.LeftArrow) && rb.velocity.z < 0f)
-			rb.AddForce (transform.forward * (moveSpeed * slowSpeed));
-		
-		if (Input.GetKey (KeyCode.RightArrow) && rb.velocity.z < maxSpeed)
-			rb.AddForce (transform.forward * moveSpeed);
-		else if (!Input.GetKey (KeyCode.RightArrow) && rb.velocity.z > 0f)
-			rb.AddForce (transform.forward * -(moveSpeed * slowSpeed));
+		nv.x = v.x + Input.GetAxis ("Vertical") * moveSpeed;
+		nv.z = v.z + Input.GetAxis ("Horizontal") * moveSpeed + slowSpeed;
 
-		if (Input.GetKey (KeyCode.Space))
-			blaster.SetActive(true);
-		else
-			blaster.SetActive (false);
+		transform.position = nv;
 	}
 }
