cf0265efcd804a51c2a4a74ea23b6295b9008679
Rudy Parenti <rudy-parenti@aliengraphics.eu>
2017-09-02 01:43:11 +0200

    Adding global scrolling
    + the level manager that control the global speed of scrolling
    + Script that set an object as following the scroll

---
+++ Assets/script/Managers/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+	private static LevelManager _instance;
+
+	[SerializeField]
+	private float standardScrollSpeed = 0f;
+
+	private void Awake ()
+	{
+		if (_instance != this && _instance != null)
+			DestroyImmediate (this);
+		_instance = this;
+	}
+
+	public float scrollSpeed
+	{
+		get { return standardScrollSpeed; }
+	}
+
+	public static LevelManager instance
+	{
+		get { return _instance; }
+	}
+}
