d0509b72d3c320837d7d24fb9be233eb8c4a5017
Rudy Parenti <rudy-parenti@aliengraphics.eu>
2017-09-03 02:05:12 +0200

    Adding a simple damaging system
    Fix some physics issues

---
+++ Assets/script/Enemy/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	[SerializeField]
+	private int maxLife = 100;
+
+	private int life;
+
+	private void Start ()
+	{
+		life = maxLife;
+	}
+
+	private void OnCollisionEnter (Collision c)
+	{
+		Bullet b = c.gameObject.GetComponent <Bullet> ();
+		if (c.collider.tag == "Player" && b)
+		{
+			DoDamage (b.Damage);
+			Destroy (c.gameObject);
+		}
+	}
+
+	private void DoDamage (int damage)
+	{
+		int nLife = life - damage;
+
+		if (nLife <= 0)
+			Destroy (this.gameObject);
+		else 
+			life = nLife;
+	}
+}
