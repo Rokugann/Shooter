b760be372074f0a61e5783e11495180c6a7ea5f9
Rudy Parenti <rudy-parenti@aliengraphics.eu>
2017-09-03 22:35:39 +0200

    Added some functions for zooming
    + Camera can now zoom and shift a little.
    + removing some components on camera and vessel

--- Assets/script/world/Camera/CameraController.cs
+++ Assets/script/world/Camera/CameraController.cs
@@ -7,18 +7,32 @@ public class CameraController : MonoBehaviour
 	[SerializeField]
 	private Vector3 topLeftCollider = new Vector3 (), botRightCollider = new Vector3 ();
 
+	[SerializeField]
+	private float zoomLevel = 30f, shift = 0.2f;
+
 	private static Vector3 trueTLCollider, trueBRCollider, tlCollider, brCollider;
+	private static float _zoomLevel, _shift = 0.2f;
 
 	private void Start ()
 	{
 		tlCollider = topLeftCollider + transform.position;
 		brCollider = botRightCollider + transform.position;
+		_zoomLevel = zoomLevel;
 	}
 
-	private void Update ()
+	private void LateUpdate ()
 	{
+		//======BOUNDARY=======
 		trueTLCollider = tlCollider + transform.position;
 		trueBRCollider = brCollider + transform.position;
+
+		//======= ZOOM =======
+		Vector3 v = transform.position;
+
+		v.y = _zoomLevel;
+		v.x = _shift * _zoomLevel;
+
+		transform.position = v;
 	}
 
 	public static Vector3 TopLeftCollider
@@ -31,6 +45,12 @@ public class CameraController : MonoBehaviour
 	{
 		get { return trueBRCollider; }
 		set { brCollider = value; }
+	}
+
+	public static float ZoomLevel
+	{
+		get { return _zoomLevel; }
+		set { _zoomLevel = value; }
 	}
 
 	private void OnDrawGizmos ()
